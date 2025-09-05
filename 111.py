980d6f6cc89ffcc69c01311d88086579bbfb9585
NOTH050 <92779751+NOTH050@users.noreply.github.com>
2025-09-05 14:05:02 +0700

    111.py

--- 111.py
+++ 111.py
@@ -519,5 +519,5 @@ def download_route():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
